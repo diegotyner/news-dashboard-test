ddca79bec35ce7e1794100e08264a6a1b31183a0
diegotyner <diego.tyner@gmail.com>
2025-07-09 16:18:01 -0700

    Next init

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+// import Nav from "@/components/nav";
+import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "News Dashboard",
+  description: "Prototype for news dashboard",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <div className="flex flex-col h-full">
+          {/* <Nav /> */}
+          <div className="app flex-grow">
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
