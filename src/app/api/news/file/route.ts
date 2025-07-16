d41ce573fd6351699da4f592b7306a0cca118d10
diegotyner <diego.tyner@gmail.com>
2025-07-16 15:42:41 -0700

    small change to file loading

--- src/app/api/news/file/route.ts
+++ src/app/api/news/file/route.ts
@@ -3,11 +3,14 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const file = await fs.readFile(
-      process.cwd() + "/src/app/top_headlines.json",
-      "utf8",
-    );
-    const data = JSON.parse(file);
+    // const file = await fs.readFile(
+    //   process.cwd() + "/src/app/top_headlines.json",
+    //   "utf8",
+    // );
+    // const data = JSON.parse(file);
+    const base = process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
+    const res = await fetch(`${base}/top_headlines.json`);
+    const data = await res.json();
     return NextResponse.json({ success: true, data });
   } catch (error) {
     console.error("Failed to read JSON:", error);
